ce08ca2048d88490ba3a94bdfc0ef79259fdf21f
Hardy Pottinger <hardy.pottinger@gmail.com>
2023-06-14 17:55:57 -0500

    WIP, now stuck on import of AbstractSiteModel

--- tests/UserSerializer_test.py
+++ tests/UserSerializer_test.py
@@ -1,16 +1,25 @@
+import sys
 import yaml
-# from unittest.mock import patch
+from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth.models import User
+print(f">>>>>>>>> sys.path = {sys.path}")
 from serializers import UserSerializer
 
 class UserSerializerTest(TestCase):
 
     def setUp(self):
+        
         with open('fixtures/users.yaml') as file:
             self.fixture_data = yaml.load(file, Loader=yaml.FullLoader)
 
-    def test_user_serializer_valid_user(self):
+    # we need to mock the models that are related to the User model
+    @patch('serializers.ArticleOrdering')
+    @patch('serializers.Issue')
+    @patch('serializers.IssueType')
+    @patch('serializers.Journal')
+    @patch('serializers.SectionOrdering')
+    def test_user_serializer_valid_user(self, mock_section_ordering, mock_journal, mock_issue_type, mock_issue, mock_article_ordering):
         valid_user_data = self.fixture_data['valid_user']
         
         serializer = UserSerializer(data=valid_user_data)
@@ -32,6 +41,11 @@ class UserSerializerTest(TestCase):
         self.assertEqual(user.first_name, valid_user_data['first_name'])
         self.assertEqual(user.last_name, valid_user_data['last_name'])
 
+    @patch('serializers.ArticleOrdering')
+    @patch('serializers.Issue')
+    @patch('serializers.IssueType')
+    @patch('serializers.Journal')
+    @patch('serializers.SectionOrdering')
     def test_user_serializer_invalid_user_missing_email(self):
         invalid_user_data = self.fixture_data['invalid_user_missing_email']
         serializer = UserSerializer(data=invalid_user_data)
@@ -39,6 +53,11 @@ class UserSerializerTest(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertIn('email', serializer.errors)
 
+    @patch('serializers.ArticleOrdering')
+    @patch('serializers.Issue')
+    @patch('serializers.IssueType')
+    @patch('serializers.Journal')
+    @patch('serializers.SectionOrdering')
     def test_user_serializer_invalid_user_missing_first_name(self):
         invalid_user_data = self.fixture_data['invalid_user_missing_first_name']
         serializer = UserSerializer(data=invalid_user_data)
@@ -46,6 +65,11 @@ class UserSerializerTest(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertIn('first_name', serializer.errors)
 
+    @patch('serializers.ArticleOrdering')
+    @patch('serializers.Issue')
+    @patch('serializers.IssueType')
+    @patch('serializers.Journal')
+    @patch('serializers.SectionOrdering')
     def test_user_serializer_invalid_user_missing_last_name(self):
         invalid_user_data = self.fixture_data['invalid_user_missing_last_name']
         serializer = UserSerializer(data=invalid_user_data)
